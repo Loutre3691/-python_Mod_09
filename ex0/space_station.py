fb6004727173167fcc12bfef605dc9631928b46e
Flora De Checchi <flora.dechecchi@gmail.com>
2026-05-01 19:36:36 +0200

    modul9 ex0, class avec les attribu, instamce de cree abvec les datas, utilisation de pydantic avec basemodel en inport et du coup pas de init pour la class spacestation car pydantic gere ca

---
+++ ex0/space_station.py
@@ -0,0 +1,29 @@
+
+from pydantic import BaseModel
+# from pydantic import ValidatorError
+
+
+class SpaceStation(BaseModel):
+        name: str
+        id: str
+        crew: int
+        power: float
+        oxygen: float
+        status: bool
+
+    
+if __name__ == "__main__":
+    print("Space Station Data Validation\n"
+          "========================================")
+
+    valid_station = SpaceStation(
+    name="ISS001",
+    id="International Space Station",
+    crew=6,
+    power=85.5,
+    oxygen=92.3,
+    status="Operational"
+    )
+
+    print(valid_station.crew)
+    
